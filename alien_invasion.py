597f335b19639551c8db631a8d3d9016b9811fe3
gky <2452444@tongji.edu.cn>
2024-12-24 17:13:24 +0800

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,18 +27,22 @@ class AlienInvation:
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            # 侦听键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # 每次循环时都重绘屏幕
-            self.screen.fill(self.bg_color)
-            self.ship.blitme()
-
-            # 让最近绘制的屏幕可⻅
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """响应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+        self.screen.fill(self.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
