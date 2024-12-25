0030f3f8c5a3e4b69074891628972871abf7f12e
gky <2452444@tongji.edu.cn>
2024-12-25 15:45:21 +0800

    create bullet.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import *
 from ship import *
+from bullet import *
 
 
 class AlienInvation:
@@ -19,6 +20,7 @@ class AlienInvation:
         pygame.display.set_caption("Alien Invation")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         # 设置背景色
         self.bg_color = self.settings.bg_color
@@ -28,6 +30,7 @@ class AlienInvation:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
 
@@ -49,6 +52,8 @@ class AlienInvation:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
 
@@ -59,9 +64,27 @@ class AlienInvation:
         elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """创建一颗子弹，并将其加入编组 bullets"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """更新子弹的位置并删除已消失的子弹"""
+        # 更新子弹的位置
+        self.bullets.update()
+
+        # 删除已消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
         self.screen.fill(self.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
 
         pygame.display.flip()
